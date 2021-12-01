f111ca178bcba9a19c5ccb084d097a6705ecec6f
EhsanAlimo <ehsan.russell69@gmail.com>
2021-12-01 11:05:57 +1100

    mobiel style to do

---
+++ warmups/week-11/01-12/main.js
@@ -0,0 +1,14 @@
+// # Cascading Style Shop
+
+// You've been asked to create a mobile website layout for an online store. The owner isn't very imaginative - here is their design:
+
+// https://drive.google.com/file/d/1w6EZy84ePE4Ik0_qp_GpAC0n4qQHXYbm/view?usp=sharing
+
+// To start off, you've been tasked with creating the mobile layout above. Your goal is to get the page to look as close as possible to the example.
+// Feel free to use a tool like tailwind, sass or semantic css if you'd prefer, but a library with premade components like MUI is cheating.
+
+// BONUS - desktop layout:
+// https://drive.google.com/file/d/1XfPkBgjfS0XIljBpHORQTynFOfxf5Ret/view?usp=sharing
+
+// Placeholder image:
+// https://drive.google.com/file/d/1HPZH-GgAFQ7mWhsJQfmhTkbu5ZXroIOP/view?usp=sharing
